c75f428cf8985e2480191134a909cb194337f017
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-04-22 15:52:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const [data, setData] = useState([])
+
+  
+  const getData = async() =>{
+    let response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json();
+    console.log(data)
+    setData(data)
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center text-xl'>KodeCamp Task 4</h1>
+      <div className='card-container'>
+        {data.map(person => (
+          <div key={person.id} className="bg-slate-100 container">
+            <h1 className='text-2xl font-bold'>{person.name}</h1>
+            <p>{person.company.catchPhrase}</p>
+            <div className='mt-4'>
+              <h3 className='text-xl font-bold'>General Info</h3>
+              <p className='text-sm'><span className='font-bold'>Username</span>: {person.username}</p>
+              <p className='text-sm'><span className='font-bold'>Address</span>: {`${person.address.street} ${person.address.suite} ${person.address.city}`}</p>
+              <p className='text-sm'><span className='font-bold'>Email</span>: <small>{person.email}</small></p>
+              <p className='text-sm'><span className='font-bold'>Phone</span>: {person.phone}</p>
+            </div>
+          </div>
+        ))}
+        
+      </div>
     </div>
+    
   );
 }
 
